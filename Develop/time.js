e5ce2e02e11021356a2529b69fd9c253a7763bc7
roddazad <roddazad@gmail.com>
2022-11-03 13:58:45 -0700

    added the html file

---
+++ Develop/time.js
@@ -0,0 +1,16 @@
+
+var today = new Date();
+var current = document.getElementById('currentDay');
+
+current.textContent = today;
+
+document.getElementById("btn-9").addEventListener('click', function(event){
+   var id = event.target.id.split('-')[1];
+//    console.log(`text-${id}`);
+   var text = document.getElementById(`text-${id}`).value
+console.log(text);
+localStorage.setItem(id, text)
+
+if (current > 9)
+})
+
